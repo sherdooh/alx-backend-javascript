eac143523733ea19a4293dd80a0025fb7a46e383
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2023-10-09 10:08:27 +0300

    Ambient Namespaces

--- 0x04-Typescript/task_3/js/main.ts
+++ 0x04-Typescript/task_3/js/main.ts
@@ -1,15 +1,1 @@
-import { RowID, RowElement } from './interface';
-import CRUD from './crud';
-
-const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-}
-
-
-const newRowID: RowID = CRUD.insertRow(row);
-row.age = 23;
-const updatedRow: RowElement = row;
-
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+'use strict';
