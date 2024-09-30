8053c86bf1998035b0d084cf6426847e77b69380
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2024-09-30 12:50:21 +0300

    Delete 0x05-Node_JS_basic directory

--- 0x05-Node_JS_basic/3-main_0.js
+++
@@ -1,9 +0,0 @@
-onst countStudents = require('./3-read_file_async');
-
-countStudents("nope.csv")
-    .then(() => {
-        console.log("Done!");
-    })
-        .catch((error) => {
-        console.log(error);
-    });
