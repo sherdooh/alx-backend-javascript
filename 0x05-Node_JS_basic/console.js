63a02ed0db9c2cfc2bffa61b353e4eaea426514a
Sherdooh <Sherkiplah@gmail.com>
2024-10-01 12:58:55 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/console.js
@@ -0,0 +1,5 @@
+fucnction displayMessage(message){
+	
+	console.log(message);
+}
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/main.js
@@ -0,0 +1,4 @@
+// 0-main.js
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
